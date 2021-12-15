bf6790c458f8a3422f08f529c2342f2eea6b42bd
Kandela77 <51201772+Kandela77@users.noreply.github.com>
2021-12-16 01:15:32 +0300

    Some exchanges added

--- python/ArbitrageV2.0.py
+++ python/ArbitrageV2.0.py
@@ -2,6 +2,7 @@ import json
 import requests
 import threading
 
+
 def coinmarket(number):
     marketc = requests.get(
         "https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active,untracked&exchangeAux=is_active,status&cryptoAux=is_active,status&start=1&limit=10000")
@@ -10,7 +11,7 @@ def coinmarket(number):
 
     stable = ["USDT", "USDC", "BUSD", "DAI", "UST", "TUSD", "USDP", "FEI", "HUSD", "WBTC", "BTCB"]
     global coins
-    coins={}
+    coins = {}
 
     for i in range(0, number):
         if marketcap['data']['cryptoCurrencyMap'][i]['symbol'] in stable:
@@ -18,128 +19,134 @@ def coinmarket(number):
         elif marketcap['data']['cryptoCurrencyMap'][i]['symbol'] == "MIOTA":
             coins["IOTA"] = marketcap['data']['cryptoCurrencyMap'][i]['slug']
         else:
-            coins[marketcap['data']['cryptoCurrencyMap'][i]['symbol']] = marketcap['data']['cryptoCurrencyMap'][i]['slug']
+            coins[marketcap['data']['cryptoCurrencyMap'][i]['symbol']] = marketcap['data']['cryptoCurrencyMap'][i][
+                'slug']
     return coins
 
 
-
-Bitstampt=""
-Cryptocom=""
+Bitstampt = ""
+Cryptocom = ""
 """Digifinex:https://api.digifinex.com/market
 LBank:https://www.lbank.info/request/tick
-Liquid:https://api.liquid.com/products?with_rate=true
 Indoex:https://masternode.indoex.io/getavailablepairs
 Probit:https://www.probit.com/api/exchange/v1/ticker"""
 
-dict={
-    "AAX":"https://api.aax.com/common/v2/market/histTicker?type=spotAll",
-    "Binance":"https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true",
-    "Bitfinex":"https://api-pub.bitfinex.com/v2/tickers?symbols=ALL",
-    "Bithumb":"https://global-api.bithumb.pro/market/data/ticker?symbol=ALL",
-    "Bitmart":"https://www.bitmart.com/api/ds/market_trade_mappings_front?local=en_US",
-    "Bittrex":"https://api.bittrex.com/v3/markets/tickers",
-    "Bybit":"https://api-testnet.bybit.com/v2/public/tickers",
-    "Coinbase":"https://www.coinbase.com/api/v2/assets/search?base=USDT&country=TR&filter=all&include_prices=true&limit=50&order=asc&page=1&query=&resolution=day&sort=rank",
-    "Coinw":"https://www.coinw.com/open/coinw/trade/partition/quotations/all",
-    "Ftx":"https://ftx.com/api/markets",
-    "Gate":"https://www.gate.io/json_svr/get_leftbar/?u=128&c=397506",
-    "Gokumarket":"https://exchange.gokumarket.com/v3/getActiveExchangeCurrencies",
-    "Huobi":"https://api.huobi.pro/market/tickers",
-    "Kraken":"https://www.kraken.com/api/internal/cryptowatch/markets/assets?asset=USDT&limit=200&assetName=new",
-    "Kucoin":"https://www.kucoin.com/_api/trade-front/market/getSymbol/USDS?lang=tr_TR",
-    "Okex":"https://www.okex.com/priapi/v5/market/mult-cup-tickers?t=1638123712053&ccys=",
-    "Wazirx":"https://x.wazirx.com/wazirx-falcon/api/v2.0/crypto_rates"
+dict = {
+    "AAX": "https://api.aax.com/common/v2/market/histTicker?type=spotAll",
+    "Binance": "https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true",
+    "Bitfinex": "https://api-pub.bitfinex.com/v2/tickers?symbols=ALL",
+    "Bithumb": "https://global-api.bithumb.pro/market/data/ticker?symbol=ALL",
+    "Bitmart": "https://www.bitmart.com/api/ds/market_trade_mappings_front?local=en_US",
+    "Bittrex": "https://api.bittrex.com/v3/markets/tickers",
+    "Bybit": "https://api-testnet.bybit.com/v2/public/tickers",
+    "Coinbase": "https://www.coinbase.com/api/v2/assets/search?base=USDT&country=TR&filter=all&include_prices=true&limit=50&order=asc&page=1&query=&resolution=day&sort=rank",
+    "Coinw": "https://www.coinw.com/open/coinw/trade/partition/quotations/all",
+    "Ftx": "https://ftx.com/api/markets",
+    "Gate": "https://www.gate.io/json_svr/get_leftbar/?u=128&c=397506",
+    "Gokumarket": "https://exchange.gokumarket.com/v3/getActiveExchangeCurrencies",
+    "Huobi": "https://api.huobi.pro/market/tickers",
+    "Kraken": "https://www.kraken.com/api/internal/cryptowatch/markets/assets?asset=USDT&limit=200&assetName=new",
+    "Kucoin": "https://www.kucoin.com/_api/trade-front/market/getSymbol/USDS?lang=tr_TR",
+    "Liquid":"https://api.liquid.com/products?with_rate=true",
+    "Probit":"https://www.probit.com/api/exchange/v1/ticker",
+    "Okex": "https://www.okex.com/priapi/v5/market/mult-cup-tickers?t=1638123712053&ccys=",
+    "Wazirx": "https://x.wazirx.com/wazirx-falcon/api/v2.0/crypto_rates"
 }
 
-dict2={"AAX":{},
-       "Binance":{},
-       "Bitfinex":{},
-       "Bithumb":{},
-       "Bitmart":{},
-       "Bittrex":{},
-       "Bybit":{},
-       "Coinbase":{},
-       "Coinw":{},
-       "Ftx":{},
-       "Gate":{},
-       "Gokumarket":{},
-       "Huobi":{},
-       "Kraken":{},
-       "Kucoin":{},
-       "Okex":{},
-       "Wazirx":{}
-       }
+dict2 = {"AAX": {},
+         "Binance": {},
+         "Bitfinex": {},
+         "Bithumb": {},
+         "Bitmart": {},
+         "Bittrex": {},
+         "Bybit": {},
+         "Coinbase": {},
+         "Coinw": {},
+         "Ftx": {},
+         "Gate": {},
+         "Gokumarket": {},
+         "Huobi": {},
+         "Kraken": {},
+         "Kucoin": {},
+         "Liquid": {},
+         "Probit": {},
+         "Okex": {},
+         "Wazirx": {}
+         }
 
-dict3={
-    "BNBBTC":"",
-    "BNBETH":"",
-    "ETHBTC":"",
-    "BTCBNB":{
-        "BTC":{},
-        "BNB":{}
+dict3 = {
+    "BNBBTC": "",
+    "BNBETH": "",
+    "ETHBTC": "",
+    "BTCBNB": {
+        "BTC": {},
+        "BNB": {}
     },
-    "BTCETH":{
-        "BTC":{},
-        "ETH":{}
+    "BTCETH": {
+        "BTC": {},
+        "ETH": {}
     },
-    "ETHBNB":{
-        "ETH":{},
-        "BNB":{}
+    "ETHBNB": {
+        "ETH": {},
+        "BNB": {}
     }
 }
-def threadd(exchangedict,coinsdict):
+
+
+def threadd(exchangedict, coinsdict):
     threads = []
     for i in exchangedict.keys():
-        t = threading.Thread(target=checkprice, args=[exchangedict[i],coinsdict])
+        t = threading.Thread(target=checkprice, args=[exchangedict[i], coinsdict])
         t.start()
         threads.append(t)
 
     for thread in threads:
         thread.join()
 
-def checkprice(url,coinsdict):
-    if url==dict['AAX']:
-        r=requests.get(url)
-        if r.status_code==200:
-            site_json=json.loads(r.content)
+
+def checkprice(url, coinsdict):
+    if url == dict['AAX']:
+        r = requests.get(url)
+        if r.status_code == 200:
+            site_json = json.loads(r.content)
             if site_json['data']['histTickers'] != None:
                 for i in coinsdict.items():
-                    newcur= i[0]+"USDT"
+                    newcur = i[0] + "USDT"
                     for y in site_json['data']['histTickers']:
                         if newcur == y['symbol']:
-                            dict2['AAX'][i[1]]=float(y['close'])
+                            dict2['AAX'][i[1]] = float(y['close'])
                             break
                         else:
-                            dict2['AAX'][i[1]]=None
+                            dict2['AAX'][i[1]] = None
             else:
                 print("Wrong URL")
         else:
             print("Wrong Url")
-    elif url==dict['Binance']:
-        r=requests.get(url)
-        if r.status_code==200:
-            site_json=json.loads(r.content)
+    elif url == dict['Binance']:
+        r = requests.get(url)
+        if r.status_code == 200:
+            site_json = json.loads(r.content)
             if site_json['data'] != None:
                 for i in coinsdict.items():
                     for y in site_json['data']:
-                        if i[0] == y['b'] and y['q'] =="USDT":
-                            dict2['Binance'][i[1]]=float(y['c'])
+                        if i[0] == y['b'] and y['q'] == "USDT":
+                            dict2['Binance'][i[1]] = float(y['c'])
                             break
                         else:
-                            dict2['Binance'][i[1]]=None
+                            dict2['Binance'][i[1]] = None
             else:
                 print("Wrong URL")
         else:
             print("Wrong Url")
-    elif url==dict['Bitfinex']:
-        r=requests.get(url)
-        if r.status_code==200:
-            site_json=json.loads(r.content)
+    elif url == dict['Bitfinex']:
+        r = requests.get(url)
+        if r.status_code == 200:
+            site_json = json.loads(r.content)
             if len(site_json) != 0:
                 for i in coinsdict.items():
-                    newcur="t"+i[0]+"USD"
+                    newcur = "t" + i[0] + "USD"
                     for y in site_json:
-                        if y[0]==newcur:
+                        if y[0] == newcur:
                             dict2['Bitfinex'][i[1]] = float(y[3])
                             break
                         else:
@@ -152,9 +159,9 @@ def checkprice(url,coinsdict):
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['info']) !=0:
+            if len(site_json['info']) != 0:
                 for i in coinsdict.items():
-                    newcur=i[0]+"-USDT"
+                    newcur = i[0] + "-USDT"
                     for y in site_json['info']:
                         if newcur == y['s']:
                             dict2['Bithumb'][i[1]] = float(y['c'])
@@ -171,7 +178,7 @@ def checkprice(url,coinsdict):
             site_json = json.loads(r.content)
             if len(site_json['data']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0]+"/USDT"
+                    newcur = i[0] + "/USDT"
                     for y in site_json['data']['result'][0]['mappingList']:
                         if newcur == y['name']:
                             dict2['Bitmart'][i[1]] = float(y['c'])
@@ -190,10 +197,10 @@ def checkprice(url,coinsdict):
                 print("Wrong Url")
             else:
                 for i in coinsdict.items():
-                    newcur=i[0]+"-USDT"
+                    newcur = i[0] + "-USDT"
                     for y in site_json:
                         if newcur == y['symbol']:
-                            dict2['Bittrex'][i[1]] = round(float(y['lastTradeRate']),7)
+                            dict2['Bittrex'][i[1]] = round(float(y['lastTradeRate']), 7)
                             break
                         else:
                             dict2['Bittrex'][i[1]] = None
@@ -205,7 +212,7 @@ def checkprice(url,coinsdict):
             site_json = json.loads(r.content)
             if len(site_json['result']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0]+"USDT"
+                    newcur = i[0] + "USDT"
                     for y in site_json['result']:
                         if y['symbol'] == newcur:
                             dict2['Bybit'][i[1]] = float(y['last_price'])
@@ -222,7 +229,7 @@ def checkprice(url,coinsdict):
                 for i in coinsdict.items():
                     for y in site_json['data']:
                         if y['base'] == i[0]:
-                            dict2['Coinbase'][i[1]] = round(float(y['latest']),7)
+                            dict2['Coinbase'][i[1]] = round(float(y['latest']), 7)
                             break
                         else:
                             dict2['Coinbase'][i[1]] = None
@@ -253,7 +260,7 @@ def checkprice(url,coinsdict):
             site_json = json.loads(r.content)
             if 'result' in site_json:
                 for i in coinsdict.items():
-                    newcur=i[0]+"/USDT"
+                    newcur = i[0] + "/USDT"
                     for y in site_json['result']:
                         if newcur == y['name']:
                             dict2['Ftx'][i[1]] = float(y['last'])
@@ -268,9 +275,9 @@ def checkprice(url,coinsdict):
         r = requests.post(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['USDT']) >0:
+            if len(site_json['USDT']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0].lower()+"_usdt"
+                    newcur = i[0].lower() + "_usdt"
                     for y in site_json['USDT']:
                         if y == newcur:
                             dict2['Gate'][i[1]] = float(site_json['USDT'][y]['rate'])
@@ -285,9 +292,9 @@ def checkprice(url,coinsdict):
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['data']) >0:
+            if len(site_json['data']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0]+"_USDT"
+                    newcur = i[0] + "_USDT"
                     for y in site_json['data']:
                         if y['currency_pair'] == newcur:
                             dict2['Gokumarket'][i[1]] = float(y['currentPrice'])
@@ -302,9 +309,9 @@ def checkprice(url,coinsdict):
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['data']) >0:
+            if len(site_json['data']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0].lower()+"usdt"
+                    newcur = i[0].lower() + "usdt"
                     for y in site_json['data']:
                         if y['symbol'] == newcur:
                             dict2['Huobi'][i[1]] = float(y['close'])
@@ -319,7 +326,7 @@ def checkprice(url,coinsdict):
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['result']) >0:
+            if len(site_json['result']) > 0:
                 for i in coinsdict.items():
                     for y in site_json['result']:
                         if y['asset'] == i[0]:
@@ -335,9 +342,9 @@ def checkprice(url,coinsdict):
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
-            if len(site_json['data']) >0:
+            if len(site_json['data']) > 0:
                 for i in coinsdict.items():
-                    newcur=i[0]+"-USDT"
+                    newcur = i[0] + "-USDT"
                     for y in site_json['data']:
                         if newcur == y['symbol']:
                             dict2['Kucoin'][i[1]] = float(y['lastTradedPrice'])
@@ -348,9 +355,40 @@ def checkprice(url,coinsdict):
                 print("Wrong Url")
         else:
             print("Wrong url")
+    elif url == dict['Liquid']:
+        r = requests.get(url)
+        if r.status_code == 200:
+            site_json = json.loads(r.content)
+            if len(site_json) > 0:
+                for i in coinsdict.items():
+                    for y in site_json:
+                        if y['quoted_currency'] == "USDT" and i[0] ==y['base_currency']:
+                            dict2['Liquid'][i[1]] = float(y['average_price'])
+                            break
+                        else:
+                            dict2['Liquid'][i[1]] = None
+            else:
+                print("Wrong URL")
+        else:
+            print("Wrong Url")
+    elif url == dict['Probit']:
+        r = requests.get(url)
+        if r.status_code == 200:
+            site_json = json.loads(r.content)
+            if 'data' in site_json:
+                for i in coinsdict.items():
+                    newcur = i[0] + "-USDT"
+                    for y in site_json['data']:
+                        if y['market_id'] == newcur:
+                            dict2['Probit'][i[1]] = float(y['last'])
+                            break
+                        else:
+                            dict2['Probit'][i[1]] = None
+        else:
+            print("Wrong url")
     elif url == dict['Okex']:
         for i in coinsdict.items():
-            url+=i[0]+","
+            url += i[0] + ","
         r = requests.get(url)
         if r.status_code == 200:
             site_json = json.loads(r.content)
@@ -374,16 +412,17 @@ def checkprice(url,coinsdict):
             for i in coinsdict.items():
                 for y in site_json:
                     if i[0].lower() == y:
-                        dict2['Wazirx'][i[1]] = round(float(site_json[y]['usdt']),7)
+                        dict2['Wazirx'][i[1]] = round(float(site_json[y]['usdt']), 7)
                         break
                     else:
                         dict2['Wazirx'][i[1]] = None
         else:
             print("WRONG URL")
-            
+
+
 def run():
     coinmarket(50)
-    threadd(dict,coins)
+    threadd(dict, coins)
     j = json.dumps(dict2)
     with open("price.json", "w+") as f:
         f.write(j)
